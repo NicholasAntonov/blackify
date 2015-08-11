c16c017004d1bbe5de6e28f0d62d970170bd35be
nicholasantonov <nicholasantonov@gmail.com>
2015-08-11 10:44:38 -0400

    Almost works aside from casting issue

--- src/blackify/core.clj
+++ src/blackify/core.clj
@@ -1,7 +1,46 @@
 (ns blackify.core
+  (:use mikera.image.colours)
+  (:use mikera.image.core)
+  (:use mikera.image.filters)
+  (:use mikera.image.spectrum)
+  (:require [clojure.java.io :refer [resource]])
+  (:import [java.awt Color])
   (:gen-class))
+
+(defn black-or-white
+  [color]
+  (.getRGB (if (> (.getGreen (new Color color)) (* 0.9 255))
+    Color/BLACK
+    Color/WHITE)))
+
+(defn blackify
+  []
+  (let [count ((grayscale) (-> "count.png" clojure.java.io/file load-image))]
+    ;;(set-pixels count (map black-or-white (get-pixels count)))
+    (show count :title "foobar")))
+
+(defn newImage
+  []
+  ;; create a new image
+  (let [bi (new-image 32 32)]
+
+    ;; gets the pixels of the image, as an int array
+    (def pixels (get-pixels bi))
+
+    ;; fill some random pixels with colours
+    (dotimes [i 1024]
+      (aset pixels i (rand-colour)))
+
+    ;; update the image with the newly changed pixel values
+    (set-pixels bi pixels)
+
+    (show bi)
+
+    (save bi "bar.png" :quality 0.9 :progressive true)))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [threshold & args]
+  (println "Hello, World!")
+  (println threshold)
+  (blackify))
