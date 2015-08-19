cfc21b5ee6279d84ad1d7504912bba7448adbb23
nicholasantonov <nicholasantonov@gmail.com>
2015-08-19 02:27:42 -0400

    Create stencil like images

--- src/blackify/core.clj
+++ src/blackify/core.clj
@@ -9,38 +9,21 @@
 
 (defn black-or-white
   [color]
-  (.getRGB (if (> (.getGreen (new Color color)) (* 0.9 255))
+  (.getRGB (if (> (.getGreen (new Color color)) (* 0.7 255))
     Color/BLACK
     Color/WHITE)))
 
 (defn blackify
   []
-  (let [count ((grayscale) (-> "count.png" clojure.java.io/file load-image))]
-    ;;(set-pixels count (map black-or-white (get-pixels count)))
+  (let [count (-> "count.png" clojure.java.io/file load-image)]
+    (set-pixels count
+      (into-array Integer/TYPE
+        (map black-or-white (get-pixels ((grayscale) count)))))
     (show count :title "foobar")))
-
-(defn newImage
-  []
-  ;; create a new image
-  (let [bi (new-image 32 32)]
-
-    ;; gets the pixels of the image, as an int array
-    (def pixels (get-pixels bi))
-
-    ;; fill some random pixels with colours
-    (dotimes [i 1024]
-      (aset pixels i (rand-colour)))
-
-    ;; update the image with the newly changed pixel values
-    (set-pixels bi pixels)
-
-    (show bi)
-
-    (save bi "bar.png" :quality 0.9 :progressive true)))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [threshold & args]
+  [& args]
   (println "Hello, World!")
-  (println threshold)
+  (println (first args))
   (blackify))
