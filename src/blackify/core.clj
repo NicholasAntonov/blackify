2a0d8ccf4d9572aa0024aafe0812cb065fac20a6
nicholasantonov <nicholasantonov@gmail.com>
2015-08-20 19:40:21 -0400

    Move the pixel mapping function

--- src/blackify/core.clj
+++ src/blackify/core.clj
@@ -4,6 +4,7 @@
   (:use mikera.image.filters)
   (:use mikera.image.spectrum)
   (:require [clojure.java.io :refer [resource]])
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:import [java.awt Color])
   (:gen-class))
 
@@ -17,17 +18,21 @@
   ;; An option with a required argument
   [["-t" "--tolerance TOLERANCE" "Tolerance"
     :default 0.5
-    :parse-fn #(Integer/parseDouble %)
+    :parse-fn #(Double/parseDouble %)
     :validate [#(< 0 % 1) "Must be a number between 0 and 1"]]
    ;; A boolean option defaulting to nil
    ["-i" "--inverted"]])
 
 (defn blackify
-  []
+  [options]
   (let [count (-> "count.png" clojure.java.io/file load-image)]
     (set-pixels count
       (into-array Integer/TYPE
-        (map black-or-white (get-pixels ((grayscale) count)))))
+        (map
+          #(.getRGB (if (< (.getGreen (new Color %)) (* 0.5 255))
+            Color/BLACK
+            Color/WHITE))
+          (get-pixels ((grayscale) count)))))
     (show count :title "foobar")))
 
 (defn -main
@@ -35,4 +40,4 @@
   [& args]
   (println "Hello, World!")
   (println (first args))
-  (blackify))
+  (blackify (parse-opts args cli-options)))
